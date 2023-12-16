6bb49e276e5c28c69264ceadd09705e4fd3f7cb0
ObukhovVladislav <vobukhov10@gmail.com>
2023-12-16 13:26:53 +0500

    add view for profile

--- forum_profile/models.py
+++ forum_profile/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 
 
@@ -8,7 +8,7 @@ class Profile(models.Model):
     username = models.CharField(max_length=16)
     description = models.TextField()
     signature = models.TextField()
-    profile_color = models.CharField(validators=[MinLengthValidator(8)], max_length=8)
+    profile_color = models.CharField(validators=[MinLengthValidator(8), RegexValidator(r'^[0-9a-f]{8}$')], max_length=8)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     class Meta:
